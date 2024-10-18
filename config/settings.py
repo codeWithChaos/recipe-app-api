b81b35516f7b68ac6fd529760f6468e53633652d
codewithchaos <codewithchaos1809@gmail.com>
2024-10-18 13:50:27 +0000

    Delete the recipe app and add core app to the project.

--- config/settings.py
+++ config/settings.py
@@ -28,7 +28,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # local apps
-    'recipe',
+    'core',
 ]
 
 MIDDLEWARE = [
@@ -113,9 +113,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, '/static/')
-# ]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, '/static/')
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field

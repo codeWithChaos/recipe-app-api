d87bc740c68e4d75ce7fdfd59d4643e8a9930425
codewithchaos <codewithchaos1809@gmail.com>
2024-10-19 14:24:47 +0000

    Add wait for db to checks.yml for github workflows.

--- core/management/commands/wait_for_db.py
+++ core/management/commands/wait_for_db.py
@@ -3,7 +3,7 @@ Django command to wait for the database to be available
 """
 from django.core.management.base import BaseCommand
 import time
-from psycopg2 import OperationalError as Psycopg2Error
+from psycopg2 import OperationalError as Psycopg2OpError
 from django.db.utils import OperationalError
 
 
@@ -19,7 +19,7 @@ class Command(BaseCommand):
             try:
                 self.check(databases=['default'])
                 db_up = True
-            except (Psycopg2Error, OperationalError):
+            except (Psycopg2OpError, OperationalError):
                 self.stdout.write('Database unavailable, waiting 1 second...')
                 time.sleep(1)
 

1e205b00ef862a959b8d195d4c3c0ae29fa693e8
codewithchaos <codewithchaos1809@gmail.com>
2024-11-04 12:36:20 +0000

    Add unit test for bad credentials.

--- user/tests/test_user_api.py
+++ user/tests/test_user_api.py
@@ -80,3 +80,13 @@ class PublicUserApiTests(TestCase):
 
         self.assertIn('token', res.data)
         self.assertEqual(res.status_code, status.HTTP_200_OK)
+
+    def test_create_token_bad_credentials(self):
+        """Test returns error if credentials invalid"""
+        create_user(email='test@example.com', password='goodpass')
+
+        payload = {'email': 'password': 'badpass'}
+        res = self.client.post(TOKEN_URL, payload)
+
+        self.assertNotIn('token', res.data)
+        self.assertEqual(res.status_code, status.HTTO_400_BAD_REQUEST)
